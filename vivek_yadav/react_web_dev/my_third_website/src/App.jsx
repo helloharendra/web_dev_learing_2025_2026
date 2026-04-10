8b02c5d6dfbbc7093e4ccf89db46ec842667ed7c
Harendra Prajapati <harendraprajapati72@gmail.com>
2026-04-10 10:11:29 +0530

    created third website

---
+++ vivek_yadav/react_web_dev/my_third_website/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/about";
+
+function App () {
+  return (
+    <>
+    <header/>
+    <Hero/>
+    <About/>
+    </>
+  );
+}
+
+export default App;
